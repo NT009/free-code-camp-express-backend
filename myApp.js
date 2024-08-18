52b581329293f7918b9fdfb3dacd44097efbff88
NT009 <nihal.t567@gmail.com>
2024-08-18 21:59:06 +0530

    challange completed

--- myApp.js
+++ myApp.js
@@ -1,8 +1,9 @@
 let express = require("express");
 let app = express();
+let bodyParser = require("body-parser");
 require("dotenv").config();
 console.log("Hello World");
-
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use((req, res, next) => {
   console.log(`${req?.method} ${req?.path} - ${req?.ip}`);
   next();
@@ -35,8 +36,15 @@ app.get("/:word/echo", (req, res) => {
   const echo = req.params.word;
   res.json({ echo });
 });
-app.get("/name", (req, res) => {
+app
+  .route("/name")
+  .get((req, res) => {
     const query = req.query;
-    res.json({ name:`${query.first} ${query.last}` });
+    res.json({ name: `${query.first} ${query.last}` });
+  })
+  .post((req, res) => {
+    const body = req.body;
+    res.json({ name: `${body.first} ${body.last}` });
   });
+
 module.exports = app;
